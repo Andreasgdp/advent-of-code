bf77117447daf1f007bc8d2283af22e5f7216791
Andreas Petersen <andreasgdp@gmail.com>
2024-12-03 06:25:20 +0100

    feat: 2024 day 3 part 2

--- 2024/golang/day03/main.go
+++ 2024/golang/day03/main.go
@@ -8,40 +8,56 @@ import (
 	"strconv"
 )
 
+func findDoAndDontAndMulInstructions(text string) []string {
+	re := regexp.MustCompile(`do\(\)|don't\(\)|mul\(\d+,\d+\)`)
+	matches := re.FindAllString(text, -1)
+	return matches
+}
+
 func main() {
 	// Open the input file
+	// file, err := os.Open("sample_input2.txt")
 	file, err := os.Open("input.txt")
-	// file, err := os.Open("sample_input1.txt")
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		return
 	}
 	defer file.Close()
 
-	// Regular expression to match valid mul(X,Y) instructions
-	re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
-
 	// Scanner to read the file line by line
 	scanner := bufio.NewScanner(file)
-	total := 0
+	var instructions []string
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		matches := re.FindAllStringSubmatch(line, -1)
-		for _, match := range matches {
-			if len(match) == 3 {
-				x, err1 := strconv.Atoi(match[1])
-				y, err2 := strconv.Atoi(match[2])
-				if err1 == nil && err2 == nil {
-					total += x * y
-				}
-			}
-		}
+		instructions = append(instructions, findDoAndDontAndMulInstructions(line)...)
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading file:", err)
 		return
+	}
+
+	// Process the collected instructions
+	total := 0
+	mulEnabled := true // mul instructions are enabled by default
+
+	for _, instruction := range instructions {
+		if instruction == "do()" {
+			mulEnabled = true
+		} else if instruction == "don't()" {
+			mulEnabled = false
+		} else if mulEnabled {
+			mulRe := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+			matches := mulRe.FindStringSubmatch(instruction)
+			if len(matches) == 3 {
+				x, err1 := strconv.Atoi(matches[1])
+				y, err2 := strconv.Atoi(matches[2])
+				if err1 == nil && err2 == nil {
+					total += x * y
+				}
+			}
+		}
 	}
 
 	fmt.Println("Total:", total)
